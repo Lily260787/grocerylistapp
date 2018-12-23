20fdd45684c3bf44e49bae80db5b8eb22b87cdce
julie <julie.da-prato@live.fr>
2018-12-23 17:00:13 +0100

    removal of unused grocery-item componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
 import { GroceryItemListComponent } from './grocery-item-list/grocery-item-list.component';
-import { GroceryItemComponent } from './grocery-item/grocery-item.component';
 import {FormsModule} from "@angular/forms";
 
 const config: SocketIoConfig = { url: 'http://localhost:4400', options: {} };
@@ -12,7 +11,6 @@ const config: SocketIoConfig = { url: 'http://localhost:4400', options: {} };
   declarations: [
     AppComponent,
     GroceryItemListComponent,
-    GroceryItemComponent
   ],
   imports: [
     BrowserModule,

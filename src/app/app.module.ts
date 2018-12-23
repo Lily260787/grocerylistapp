7da660a4a296102d0133930532ef5aaa3dbcd365
julie <julie.da-prato@live.fr>
2018-12-23 09:30:23 +0100

    angular app + components + service + model created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroceryItemListComponent } from './grocery-item-list/grocery-item-list.component';
+import { GroceryItemComponent } from './grocery-item/grocery-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryItemListComponent,
+    GroceryItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grocery-item-list/grocery-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-item-list',
+  templateUrl: './grocery-item-list.component.html',
+  styleUrls: ['./grocery-item-list.component.css']
+})
+export class GroceryItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
